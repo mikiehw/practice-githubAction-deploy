55196e8b501962dd80c290c77c39c7723ec27e8b
hyunwoojo <hwchokim95@hanmail.net>
2023-04-04 15:13:53 +0900

    commit_5_hellocontroller수정

--- src/main/java/com/codestates/controller/HelloController.java
+++ src/main/java/com/codestates/controller/HelloController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping
 public class HelloController {
-    @GetMapping
+    @GetMapping("/")
     public String hello() {
         return "Hello World! Hello BE Bootcamp!";
     }
